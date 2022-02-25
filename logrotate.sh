e2def7f1aa26e5a9cee84218cba423c86bcb10d7
SimoneBr55 <39730275+SimoneBr55@users.noreply.github.com>
2022-02-25 14:11:10 +0100

    Completed logrotate.sh v1.0

--- logrotate.sh
+++ logrotate.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+#-----------------------------------------------------#
+# logrotate.sh v1.0
+# > Simone Brazioli
+# Script to stash away the logs in a gz compressed file
+#-----------------------------------------------------#
+cd /home/pi/furnaceControl
+cd oldlog
+gzip -d dnsapi.ark.log.gz
+cd /home/pi/furnaceControl
+cat dnsapi.log >> oldlog/dnsapi.ark.log
+truncate -s0 dnsapi.log
+cd oldlog
+gzip dnsapi.ark.log
+exit 0
 
